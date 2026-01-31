df5f9331855e320fd628b5a5f93b8617497b53c5
Anish kath_baniya <anishk2772@gmail.com>
2026-01-31 16:21:24 +0545

    done

--- index.js
+++ index.js
@@ -5,10 +5,35 @@ if (age >= 18) {
     console.log("You are a minor.");
 }
 // This code checks if the age is 18 or older and logs a message accordingly.
-const name = "Alice";
+const name1 = "Alice";
 const age = 30;
 const dob = '10/15/1993';
 
 function sayName(){
-    console.log("My name is " + name);
+    console.log("My name is " + name1);
+
 }
+//function to say name
+function sayAge(){
+    console.log("My age is " + age);
+}
+function sayDOB(){
+    console.log("My date of birth is " + dob);
+}   
+sayName();
+sayAge();
+sayDOB(); 
+module.exports = {
+    sayName,
+    sayAge,
+    sayDOB
+};
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide
+};
+
+
+
